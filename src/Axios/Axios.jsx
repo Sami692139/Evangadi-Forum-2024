6e995225a908f062c66366aafcccfa4521e161dd
sami692139 <samuel.tassew2020@gmail.com>
2024-12-25 12:06:42 +0300

    forum

---
+++ src/Axios/Axios.jsx
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+//Axios instance for making http request
+const axiosBase = axios.create({
+  baseURL: "http://localhost:7777/",
+  // baseURL: "https://evangadi-api-jqaq.onrender.com/",
+});
+export default axiosBase;
