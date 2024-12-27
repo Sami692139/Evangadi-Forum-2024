0a010ea5b331c3b5f4cd14eb8079e65dae902f11
sami692139 <samuel.tassew2020@gmail.com>
2024-12-27 09:48:17 +0300

    Axios

--- src/Axios/Axios.jsx
+++ src/Axios/Axios.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 
 //Axios instance for making http request
 const axiosBase = axios.create({
-  baseURL: "http://localhost:7777/",
-  // baseURL: "https://evangadi-api-jqaq.onrender.com/",
+  // baseURL: "http://localhost:7777/",
+  baseURL: "https://evangadi-api-jqaq.onrender.com/",
 });
 export default axiosBase;
